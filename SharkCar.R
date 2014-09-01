ed65685b1bbad5c8864e02971237a26fdd9b9c2a
Ben Hamner <ben@benhamner.com>
2014-09-01 07:35:53 -0700

    finished plot

--- SharkCar.R
+++ SharkCar.R
@@ -1,15 +1,18 @@
 library(ggplot2)
 library(scales)
 
-df <- data.frame(Object         = factor(c("Sharks","Sharks","Cars","Cars"), levels=c("Sharks", "Cars")),
+df <- data.frame(Object         = factor(c("Sharks   ","Sharks   ","Cars","Cars"), levels=c("Sharks   ", "Cars")),
                  Action         = factor(c("Scared Of","Killed By","Scared Of","Killed By"), levels=c("Scared Of", "Killed By")),
-                 NumberOfPeople = c(313e6, 2, 1, 33500))
+                 NumberOfPeople = c(313e6, 2, 2, 33500))
 
 ggplot(data=df, aes(x=Action, y=NumberOfPeople, fill=Object)) + 
     geom_bar(stat="identity", position=position_dodge()) +
-    theme_grey(base_size=18) +
+    theme_grey(base_size=20) +
     scale_y_log10(labels=comma) +
     ylab("Number of People") +
     xlab("") +
-    theme(legend.position="bottom", legend.direction="horizontal") +
-    scale_fill_discrete("")
+    theme(legend.position="bottom",
+          legend.direction="horizontal",
+          plot.margin = unit(c(1,1,1,1), "cm")) + 
+    scale_fill_manual("", values = c("#004358","#FD7400","#00A388","#FF6138","#047C8C","#F1706D")) +
+    ggtitle("Fear of Sharks and Cars in the US")
