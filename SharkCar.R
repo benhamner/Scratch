5cc596e3aa32b71a45c09cb0d55e1794c69d40f5
Ben Hamner <ben@benhamner.com>
2014-08-31 23:09:47 -0700

    minor update

--- SharkCar.R
+++ SharkCar.R
@@ -1,7 +1,15 @@
+library(ggplot2)
+library(scales)
+
 df <- data.frame(Object         = factor(c("Sharks","Sharks","Cars","Cars"), levels=c("Sharks", "Cars")),
                  Action         = factor(c("Scared Of","Killed By","Scared Of","Killed By"), levels=c("Scared Of", "Killed By")),
-                 NumberOfPeople = c(6e9, 1, 1, 1e6))
+                 NumberOfPeople = c(313e6, 2, 1, 33500))
 
 ggplot(data=df, aes(x=Action, y=NumberOfPeople, fill=Object)) + 
     geom_bar(stat="identity", position=position_dodge()) +
-    scale_y_log10()
+    theme_grey(base_size=18) +
+    scale_y_log10(labels=comma) +
+    ylab("Number of People") +
+    xlab("") +
+    theme(legend.position="bottom", legend.direction="horizontal") +
+    scale_fill_discrete("")
