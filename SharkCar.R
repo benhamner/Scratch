d9a9c15b750c88c05b49ee0a3eaef5683a320e39
Ben Hamner <ben@benhamner.com>
2014-09-02 07:36:14 -0700

    finished plot

--- SharkCar.R
+++ SharkCar.R
@@ -8,11 +8,16 @@ df <- data.frame(Object         = factor(c("Sharks   ","Sharks   ","Cars","Cars"
 ggplot(data=df, aes(x=Action, y=NumberOfPeople, fill=Object)) + 
     geom_bar(stat="identity", position=position_dodge()) +
     theme_grey(base_size=20) +
-    scale_y_log10(labels=comma) +
+    scale_y_log10(labels=comma, breaks=c(1,1000,1e6,3e8)) +
     ylab("Number of People") +
     xlab("") +
     theme(legend.position="bottom",
           legend.direction="horizontal",
+          plot.title = element_text(vjust=3),
+          axis.text.x = element_text(color="#000000"),
           plot.margin = unit(c(1,1,1,1), "cm")) + 
     scale_fill_manual("", values = c("#004358","#FD7400","#00A388","#FF6138","#047C8C","#F1706D")) +
     ggtitle("Fear of Sharks and Cars in the US")
+
+    ggsave("SharkCar.png", width=8, height=6, units="in")
+    
