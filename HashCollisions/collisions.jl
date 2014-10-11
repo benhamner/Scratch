3d90b9eba8a522f6602ac1bd0b05d3cd615d8919
Ben Hamner <ben@benhamner.com>
2014-10-11 16:09:06 -0700

    added notes

--- HashCollisions/collisions.jl
+++ HashCollisions/collisions.jl
@@ -1,6 +1,9 @@
 using DataFrames
 using Gadfly
 
+# This was just me fiddling for fun
+# HyperLogLog would be how to do this for real http://blog.notdot.net/2012/09/Dam-Cool-Algorithms-Cardinality-Estimation
+
 # buckets[n] contains the probability that n buckets will be non-empty after num_items have been randomly inserted into the buckets 
 function count_nonempty_buckets(num_buckets, num_items)
     buckets = zeros(num_buckets)
@@ -40,6 +43,39 @@ function plot_item_distribution(num_buckets, filename="buckets.png")
     data
 end
 
+function num_items_expected(num_buckets, num_nonempty_buckets, num_samples=1000)
+    min_items_to_sample = zeros(num_samples)
+    max_items_to_sample = zeros(num_samples)
+
+    for i=1:num_samples
+        items = 0
+        nonempty_buckets = 0
+        buckets = zeros(num_buckets)
+        while nonempty_buckets <= num_nonempty_buckets
+            items = items + 1
+            bucket = rand(1:num_buckets)
+            if buckets[bucket]==0
+                buckets[bucket] = 1
+                nonempty_buckets = nonempty_buckets + 1
+                if nonempty_buckets == num_nonempty_buckets
+                    min_items_to_sample[i] = items
+                end
+            end
+        end
+        max_items_to_sample[i] = items-1
+    end
+    println("Number of items expected for ", num_buckets, " buckets with ", num_nonempty_buckets, " not empty.")
+    println("\t - Min    min items to sample: ", minimum(min_items_to_sample))
+    println("\t - Median min items to sample: ", median(min_items_to_sample))
+    println("\t - Median max items to sample: ", median(max_items_to_sample))
+    println("\t - Max    max items to sample: ", maximum(max_items_to_sample))
+    println("\t - 05th % min items to sample: ", quantile(min_items_to_sample, 0.05))
+    println("\t - 05th % max items to sample: ", quantile(max_items_to_sample, 0.05))
+    println("\t - 95th % min items to sample: ", quantile(min_items_to_sample, 0.95))
+    println("\t - 95th % max items to sample: ", quantile(max_items_to_sample, 0.95))
+    println("\t - Mean difference", mean(max_items_to_sample .- min_items_to_sample))
+end
+
 buckets = count_nonempty_buckets(128, 256)
 println(buckets)
 
@@ -47,5 +83,10 @@ println("05th %: ", minimum(find(cumsum(buckets).>0.05)))
 println("50th %: ", minimum(find(cumsum(buckets).>0.5)))
 println("95th %: ", minimum(find(cumsum(buckets).>0.95)))
 
+@time num_items_expected(128, 64)
+@time num_items_expected(16384, 100)
+@time num_items_expected(16384, 8192)
+
 @time plot_item_distribution(256, "buckets_256.png")
 @time plot_item_distribution(2^14, "buckets_16384.png")
+
