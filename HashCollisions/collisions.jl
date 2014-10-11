d42c77793edf65e67ac066eb255115e0d127f4d3
Ben Hamner <ben@benhamner.com>
2014-10-11 15:33:00 -0700

    graphed collisions results

--- HashCollisions/collisions.jl
+++ HashCollisions/collisions.jl
@@ -1,3 +1,5 @@
+using DataFrames
+using Gadfly
 
 # buckets[n] contains the probability that n buckets will be non-empty after num_items have been randomly inserted into the buckets 
 function count_nonempty_buckets(num_buckets, num_items)
@@ -12,6 +14,32 @@ function count_nonempty_buckets(num_buckets, num_items)
     buckets
 end
 
+function plot_item_distribution(num_buckets, filename="buckets.png")
+    num_items = 3*num_buckets
+
+    buckets = zeros(num_buckets)
+    buckets[1] = 1.0
+    counts = [1:length(buckets)]
+
+    p05 = Float64[]
+    p50 = Float64[]
+    p95 = Float64[]
+
+    for i=2:num_items
+        buckets = buckets .* counts / num_buckets + vcat(0, buckets[1:end-1]) .* reverse(counts) / num_buckets
+        push!(p05, minimum(find(cumsum(buckets).>0.05)))
+        push!(p50, minimum(find(cumsum(buckets).>0.50)))
+        push!(p95, minimum(find(cumsum(buckets).>0.95)))
+    end
+
+    items = [2:num_items]
+    data = DataFrame(Items=items, P05=p05, P50=p50, P95=p95)
+    draw(PNG(filename, 8inch, 6inch), plot(melt(data, [:Items]), x=:Items, y=:value, color=:variable, Geom.line))
+
+    @assert abs(sum(buckets)-1)<1e-10
+    data
+end
+
 buckets = count_nonempty_buckets(128, 256)
 println(buckets)
 
@@ -19,8 +47,5 @@ println("05th %: ", minimum(find(cumsum(buckets).>0.05)))
 println("50th %: ", minimum(find(cumsum(buckets).>0.5)))
 println("95th %: ", minimum(find(cumsum(buckets).>0.95)))
 
-# 5 items, 4 buckets
-# 1: P(1) * 1/4
-# 2: P(2) * 2/4 + P(1)*3/4
-# 3: P(3) * 3/4
-# 4: P(4) + P(3) * 1/4
+@time plot_item_distribution(256, "buckets_256.png")
+@time plot_item_distribution(2^14, "buckets_16384.png")
